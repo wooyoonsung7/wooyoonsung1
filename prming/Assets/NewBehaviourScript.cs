7e040294de3fb23cbf3b2b2e543f8c7e3eb8caec
wooyoonsung7 <suger8831@gmail.com>
2024-04-23 17:00:16 +0900

    prming

---
+++ prming/Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {       
+        string stars;
+        for (int n = 0; n < 10; n++)
+        {
+            stars = "";
+            for (int i = 0; i < 10 - n; i++)
+            {
+                stars += " ";
+            }
+            for (int o = 0; o < n + 1; o++)
+            {
+                stars += "*";
+            }
+            Debug.Log(stars);   
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
